13e1ac4bc0106455d33b1c5a90bc2ff14f490414
steve-kim-96 <soungku@hotmail.com>
2021-06-01 22:41:32 +1200

    create resolver folder for graphql

---
+++ server/src/resolvers/post.ts
@@ -0,0 +1,7 @@
+// import { Post } from "src/entities/Post";
+// import { Mutation, Query, Resolver } from "type-graphql";
+
+// @Resolver()
+// class PostResolver {
+//     @Mutation(() => Post)
+// }
