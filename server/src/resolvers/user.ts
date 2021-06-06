5706abd29ac1db5a2c111258408b6203808a0362
steve-kim <soungku@hotmail.com>
2021-06-06 12:56:03 +1200

    create user resolver

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,17 @@
+import { UserInput } from "src/inputs/userInput";
+import { Arg, Mutation, Resolver } from "type-graphql";
+import { User } from "../entities/User";
+import * as argon2 from "argon2";
+
+@Resolver()
+export class PostResolver {
+  @Mutation(() => User)
+  async register(@Arg("input") input: UserInput): Promise<User> {
+    const password = await argon2.hash(input.password);
+    return User.create({
+      username: input.username,
+      email: input.email,
+      password,
+    }).save();
+  }
+}
