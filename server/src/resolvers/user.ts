670dba8d8fd1d4a806e5031052b8512aab8dbf7c
steve-kim <soungku@hotmail.com>
2021-06-08 15:36:44 +1200

    create login mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -2,6 +2,7 @@ import { UserInput } from "../inputs/userInput";
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import * as argon2 from "argon2";
+import { UserResponse } from "../errors/userResponse";
 
 @Resolver()
 export class UserResolver {
@@ -13,5 +14,35 @@ export class UserResolver {
       email: input.email,
       password,
     }).save();
+  }
+  @Mutation(() => User)
+  async login(
+    @Arg("usernameOrEmail") usernameOrEmail: string,
+    @Arg("password") password: string
+  ): Promise<UserResponse> {
+    const user = await User.findOne(
+      usernameOrEmail.includes("@")
+        ? { where: { email: usernameOrEmail } }
+        : { where: { username: usernameOrEmail } }
+    );
+    if (!user) {
+      return {
+        errors: [
+          { field: "usernameOrEmail", message: "The username does not exists" },
+        ],
+      };
+    }
+    const verifyPassword = await argon2.verify(user.password, password);
+    if (!verifyPassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrect password",
+          },
+        ],
+      };
+    }
+    return { user };
   }
 }
